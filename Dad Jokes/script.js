2fa8e541bc2b1f9b3119087c6e1dcf18debcee2b
Büşra Tuğul <134439412+busratugul@users.noreply.github.com>
2023-07-15 11:38:20 +0300

    50ProjectsIn50Days
    Dad Jokes

--- Dad Jokes/script.js
+++ Dad Jokes/script.js
@@ -4,17 +4,31 @@ const jokeBtn = document.getElementById("jokeBtn")
 jokeBtn.addEventListener("click", generateJoke)
 generateJoke()
 
-function generateJoke() {
-    const config = {
+async function generateJoke() {
+     const config = {
         headers: {
         Accept : "application/json",
         },
     }
+ 
 
-     fetch("https://icanhazdadjoke.com", config)
-     .then((res) => res.json())
-    .then((data) => {
-        jokeEl.innerHTML = data.joke
-    }) 
+const res = await fetch("https://icanhazdadjoke.com", config)
+
+const data = await res.json()
+
+jokeEl.innerHTML = data.joke
+
 }
 
+
+//function generateJoke() {
+    //     const config = {
+    //         headers: {
+    //         Accept : "application/json",
+    //         },
+    //     }
+
+//   .then((res) => res.json())
+//     .then((data) => {
+//         jokeEl.innerHTML = data.joke
+//     }) 
